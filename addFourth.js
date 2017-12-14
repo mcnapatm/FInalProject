25ca7227831b98571cd6de79321639c565328b46
mcnapatm <mcnpatm@dunwoody.edu>
2017-12-14 08:30:47 -0600

    Delete addFourth.js

--- addFourth.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-function addFourth(){
-	document.getElementById("fourthPriority").style.display = "block";
-	document.getElementById("prioritySubmitTwo").style.display = "block";
-	document.getElementById("prioritySubmitOne").style.display = "none";
-}
