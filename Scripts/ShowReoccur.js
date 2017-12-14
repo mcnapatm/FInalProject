b928d191299b3b1dafc641a803ba0c83ac50880e
mcnapatm <mcnpatm@dunwoody.edu>
2017-12-14 08:31:29 -0600

    Add files via upload

---
+++ Scripts/ShowReoccur.js
@@ -0,0 +1,7 @@
+//Show the Reoccurance days on click
+function ShowReoccur(){
+	document.getElementById("addReoccuranceDays").style.display = "block";
+	document.getElementById("editReoccuranceDays").style.display = "block";
+	
+	
+}
