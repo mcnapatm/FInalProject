72174208f251aec3fde89734e781212bb4902513
mcnapatm <mcnpatm@dunwoody.edu>
2017-12-14 08:30:54 -0600

    Delete addFifth.js

--- addFifth.js
+++
@@ -1,6 +0,0 @@
-// JavaScript Document
-function addFifth(){
-	document.getElementById("prioritySubmitTwo").style.display = "none";
-	document.getElementById("FifthPriority").style.display = "block";
-	document.getElementById("prioritySubmitThree").style.display = "block";
-}
