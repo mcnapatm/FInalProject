6bd7dda37df8c0becdd2227fd478a3c62ec7bf72
mcnapatm <mcnpatm@dunwoody.edu>
2017-12-14 08:29:42 -0600

    Delete HideReoccur.js

--- HideReoccur.js
+++
@@ -1,6 +0,0 @@
-//Show the Reoccurance days on click
-function HideReoccur(){
-	document.getElementById("addReoccuranceDays").style.display = "none";
-	document.getElementById("editReoccuranceDays").style.display = "none";
-	
-}
